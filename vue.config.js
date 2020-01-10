a95c1c1588ae33f857dd69bb7c4e1d977b8a74fd
zhyiwen <zhang92107@sina.com>
2020-01-10 08:55:59 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-pxtorem')({
+            rootValue: 100,
+            propWhiteList: [],
+            minPixelValue: 2
+          }),
+          require('autoprefixer')()
+        ]
+      }
+    }
+  },
+}
